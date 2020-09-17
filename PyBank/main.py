a8895c466513a0493642fca5026b3e53db56c833
clmatherson <70039543+clmatherson@users.noreply.github.com>
2020-09-17 19:09:15 -0400

    Update Files and Push Changes to GitHub

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,79 +2,79 @@
 import os
 import csv
 
+# Set path to open/write csv file 
 csvpath = os.path.join('..','PyBank','Resources','budget_data.csv')
 output_file = os.path.join('..','PyBank','Analysis','budget_analysis.csv')
 
+# Create new lists for Dates and Amounts
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     headers = next(csvreader)
 
     Date = []
-    Data = []
+    Amts = []
     for row in csvreader:
-        # Data.append(row[0])
         Date.append(str(row[0]))
-        Data.append(int(row[1]))
+        Amts.append(int(row[1]))
 
+# Calculat the 'change in periods' and assign amounts to new list
 Diff_List=[]
 count=0
-for num in Data:
+for num in Amts:
     if count == 0:
         pass
     else:
-        Diffs = num - Data[count - 1]
+        Diffs = num - Amts[count - 1]
         Diff_List.append(Diffs)
     count = count + 1
 
-def average(numbers):
-    length = len(Diff_List)
-    total = 0.0
-    for number in numbers:
-        total +=number
-    return total / length
+# Calculate the average
+average1 = sum(Diff_List)/len(Diff_List)
 
-def sum(numbers):
-    total = 0
-    for num in numbers:
-        total += num    
-    return total
-
+# Count the number of periods
 lnegth_Mths = len(Date)
 
-Net_Amt = sum(Data)
+# Calculate period total
+Net_Amt = sum(Amts)
 
+# Calculate 'change in periods' total
 Total_Diff = sum(Diff_List)
 
+# Get the Min and Max and their corresponding index numbers from the 'change in periods'
 GIPmax = max(Diff_List)
 GIPmaxID = Diff_List.index(GIPmax) + 1
 
 GIPmin = min(Diff_List)
 GIPminID = Diff_List.index(GIPmin) + 1
 
+# Create a variant for the report header
 FAna = ('Financial Analysis')
 
+# Set variant and integer for data separator 
 P_Break = "-"
 PB_Repeat = 50
 
+# Set variants for each results line
 R1_Line = (f"Total Months: {lnegth_Mths}")
 R2_Line = (f"Total: ${Net_Amt}")
-R3_Line = (f"Average Change: ${average(Diff_List):.2f}")
+R3_Line = (f"Average Change: ${average1:.2f}")
 R4_Line = (f"Greatest Increase in Profits: {Date[GIPmaxID]} (${GIPmax})")
 R5_Line = (f"Greatest Decrease in Profits: {Date[GIPminID]} (${GIPmin})")
 
+# Assign results to a list
 Rpt_Summary = [R1_Line,R2_Line,R3_Line,R4_Line,R5_Line]
 
 # Print Resutls to "Budget_Analysis" File in the Analysis Folder
 with open(output_file, 'w') as csvfile2:
     csvwriter = csv.writer(csvfile2, delimiter='\n')
-    csvwriter.writerow([FAna])
+    csvfile2.write(f'{FAna}\n\n')
+    csvfile2.write(f'{str(P_Break)*PB_Repeat}\n\n')
     csvwriter.writerow(Rpt_Summary)
+    csvfile2.write(f'{str(P_Break)*PB_Repeat}')
 
 # Print Results Summary to Terminal
-print("")
-print(str(FAna))
+print(f'\n\n{str(FAna)}\n')
 print(str(P_Break)*PB_Repeat)
 for row in Rpt_Summary:
     print(row)
-print(str(P_Break)*PB_Repeat)
-print("")
+print(f'{str(P_Break)*PB_Repeat}\n\n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,9 +2,11 @@
 import os
 import csv
 
+# Set path to open/write csv file 
 csvpath = os.path.join('..','PyPoll','Resources','election_data.csv')
 output_file = os.path.join('..','PyPoll','Analysis','election_results.csv')
 
+# Create new lists for Voter ID and candidates
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     headers = next(csvreader)
@@ -15,13 +17,16 @@ with open(csvpath) as csvfile:
         Vid.append(str(row[0]))
         Candi.append(str(row[2]))
 
+# Make candidate list a 'set' to get a list with only unique records
 Candis=list(set(Candi))
 
+# Assign each candidate to a variant
 Cand_00 = Candis[0]
 Cand_01 = Candis[1]
 Cand_02 = Candis[2]
 Cand_03 = Candis[3]
 
+# Count how many votes each received and assign totals to variants
 Cand_Ct_00 = 0
 Cand_Ct_01 = 0
 Cand_Ct_02 = 0
@@ -37,51 +42,47 @@ for nam in Candi:
     if nam == Cand_03:
         Cand_Ct_03 = Cand_Ct_03 + 1
 
+# Calculate what Percentage of the vote each candidate received
 Tot_Votes = len(Vid)
+Per_VCand_00 = Cand_Ct_00 / Tot_Votes * 100
+Per_VCand_01 = Cand_Ct_01 / Tot_Votes * 100
+Per_VCand_02 = Cand_Ct_02 / Tot_Votes * 100
+Per_VCand_03 = Cand_Ct_03 / Tot_Votes * 100
 
-Tot_VCand_00 = Cand_Ct_00
-Tot_VCand_01 = Cand_Ct_01
-Tot_VCand_02 = Cand_Ct_02
-Tot_VCand_03 = Cand_Ct_03
+# Create a list with the total votes each candidate received
+Tot_VList = [Cand_Ct_00,Cand_Ct_01,Cand_Ct_02,Cand_Ct_03]
 
-Per_VCand_00 = Tot_VCand_00 / Tot_Votes * 100
-Per_VCand_01 = Tot_VCand_01 / Tot_Votes * 100
-Per_VCand_02 = Tot_VCand_02 / Tot_Votes * 100
-Per_VCand_03 = Tot_VCand_03 / Tot_Votes * 100
-
-Tot_VList = [Tot_VCand_00,Tot_VCand_01,Tot_VCand_02,Tot_VCand_03]
-
+# Get the index for the candidate with the most votes
 Most_V = max(Tot_VList)
 Winner_000 = Tot_VList.index(Most_V)
 
+# Set variant and integer for data separator 
 P_Break = "-"
 PB_Repeat = 35
 
-print('')
-print('Election Results')
-print(str(P_Break)*PB_Repeat)
+# Print Results to User Terminal
+print('\n\nElection Results')
+print(f'{str(P_Break)*PB_Repeat}')
 print(f'Total Votes : {Tot_Votes}')
-print(str(P_Break)*PB_Repeat)
-print(f'{Cand_00}: {Per_VCand_00:.3f}%  ({Tot_VCand_00})')
-print(f'{Cand_01}: {Per_VCand_01:.3f}%  ({Tot_VCand_01})')
-print(f'{Cand_02}: {Per_VCand_02:.3f}%  ({Tot_VCand_02})')
-print(f'{Cand_03}: {Per_VCand_03:.3f}%  ({Tot_VCand_03})')
-print(str(P_Break)*PB_Repeat)
+print(f'{str(P_Break)*PB_Repeat}')
+print(f'{Cand_00}: {Per_VCand_00:.3f}%  ({Cand_Ct_00})')
+print(f'{Cand_01}: {Per_VCand_01:.3f}%  ({Cand_Ct_01})')
+print(f'{Cand_02}: {Per_VCand_02:.3f}%  ({Cand_Ct_02})')
+print(f'{Cand_03}: {Per_VCand_03:.3f}%  ({Cand_Ct_03})')
+print(f'{str(P_Break)*PB_Repeat}')
 print(f'Winner: {Candis[Winner_000]}')
-print(str(P_Break)*PB_Repeat)
-print('')
+print(f'{str(P_Break)*PB_Repeat}\n\n')
 
 # Print Resutls to "election_results.csv" File in the Analysis Folder
 with open(output_file, 'w') as csvfile2:
-    csvwriter = csv.writer(csvfile2, delimiter=',')
-    csvwriter.writerow(['Election Results'])
-    csvwriter.writerow(['--------------------------'])
-    csvwriter.writerow([f'Total Votes : {Tot_Votes}'])
-    csvwriter.writerow(['--------------------------'])
-    csvwriter.writerow([f'{Cand_00}: {Per_VCand_00:.3f}%  ({Tot_VCand_00})'])
-    csvwriter.writerow([f'{Cand_01}: {Per_VCand_01:.3f}%  ({Tot_VCand_01})'])
-    csvwriter.writerow([f'{Cand_02}: {Per_VCand_02:.3f}%  ({Tot_VCand_02})'])
-    csvwriter.writerow([f'{Cand_03}: {Per_VCand_03:.3f}%  ({Tot_VCand_03})'])
-    csvwriter.writerow(['--------------------------'])
-    csvwriter.writerow([f'Winner: {Candis[Winner_000]}'])
-    csvwriter.writerow(['--------------------------'])
+    csvfile2.write('Election Results\n')
+    csvfile2.write(f'{str(P_Break)*PB_Repeat}\n')
+    csvfile2.write(f'Total Votes : {Tot_Votes}\n')
+    csvfile2.write(f'{str(P_Break)*PB_Repeat}\n')
+    csvfile2.write(f'{Cand_00}: {Per_VCand_00:.3f}%  ({Cand_Ct_00})\n')
+    csvfile2.write(f'{Cand_01}: {Per_VCand_01:.3f}%  ({Cand_Ct_01})\n')
+    csvfile2.write(f'{Cand_02}: {Per_VCand_02:.3f}%  ({Cand_Ct_02})\n')
+    csvfile2.write(f'{Cand_03}: {Per_VCand_03:.3f}%  ({Cand_Ct_03})\n')
+    csvfile2.write(f'{str(P_Break)*PB_Repeat}\n')
+    csvfile2.write(f'Winner: {Candis[Winner_000]}\n')
+    csvfile2.write(f'{str(P_Break)*PB_Repeat}')
